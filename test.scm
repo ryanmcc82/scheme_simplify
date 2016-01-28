5c3e8242afcba0ecb646fa70cc64343b97c4a727
Ryan McConn <ryanmcc@uab.edu>
2016-01-28 00:13:30 -0600

    first case passed

--- test.scm
+++ test.scm
@@ -1,24 +1,24 @@
 (define (test)
   (begin
-    (display "test1: ")
+    (display "test1: (simplify '1) \n")
     (display (test1) )
 
     (newline)
-    (display "test2: ")
+    (display "test2: simplify '(+ 1 3)\n")
     (display (test2) )
 
     (newline)
-    (display "test3: ")
+    (display "test3: (simplify '(+ 1 v))\n")
     (display (test3) )
 
     (newline)
-    (display "test4: ")
+    (display "test4: (simplify '(+ (+ 2 v) (+ 1 3) ))\n")
     (display (test4) )
 ))
 
 (define (test1) (simplify '1))
 
-(define (test2) simplify '(+ 1 3))
+(define (test2) (simplify '(+ 1 3)))
 
 (define (test3) (simplify '(+ 1 v)))
 
