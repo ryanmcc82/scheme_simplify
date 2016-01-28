233c07c213ef66de99cf0356cad801bdcafc5f4c
Ebitimi <jagbereb@uab.edu>
2016-01-28 12:44:38 -0600

    asked for a commit

--- test.scm
+++ test.scm
@@ -20,6 +20,6 @@
 
 (define (test2) simplify '(+ 1 3))
 
-(define (test3) (simplify '(+ 1 v)))
+(define (test3) ( let (v 3) (simplify '(+ 1 v))))
 
 (define (test4) (simplify '(+ (+ 2 v) (+ 1 3) )))
